ef48f75680704b356eec5cf6f180cf5ca235d02d
Pete Hawkins <pete@phawk.co.uk>
2011-11-30 10:52:33 +0000

    Updated jQuery to 1.7.1

--- app/views/template.php
+++ app/views/template.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="<?=base_url('/css/layout.css');?>" type="text/css" />
 <?= $_styles ?>
 
-<script src="<?=base_url('js/jquery-1.6.4.min.js')?>"></script>
+<script src="<?=base_url('js/jquery-1.7.1.min.js')?>"></script>
 <script src="<?=base_url('js/jquery.tools.min.js')?>"></script>
 <?= $_scripts ?>
 </head>
